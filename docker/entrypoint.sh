857cba5706d538b373740329c51c87d2031a000a
NineTrier <ramenskya@cdep.ru>
2025-10-16 14:12:36 +0300

    3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -32,6 +32,7 @@ else
   cd /app
 fi
 
+python manage.py makemigrations --noinput
 # Apply migrations.
 python manage.py migrate --noinput
 
@@ -107,7 +108,7 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin1")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin")
 
